366e4551c3f75dbd32d0c71b971b87dcdc776919
21130453-HuynhBaoHuyenNgoc <21130453@st.hcmuaf.edu.vn>
2025-03-23 11:17:03 +0700

    up satus view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import HomePage from './pages/HomePage'
 import { Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import Status from './pages/Status'
+import StatusViewer from './pages/StatusViewer'
 
 
 
@@ -14,6 +16,8 @@ function App() {
       <Route path = "/" element = {<HomePage/>}></Route>
       <Route path = "/signin" element = {<SignIn/>}></Route>
       <Route path = "/signup" element={<SignUp/>}></Route>
+      <Route path = "/status" element = {<Status/>}></Route>
+      <Route path = "/status/:userId" element = {<StatusViewer/>}></Route>
     </Routes>
    </div>
   )
